9958f8a183eb708c8a4f38e27d8652e572a77f63
DarkPhantom1805 <abdullahyousuf1234@gmail.com>
2021-11-16 18:23:59 +0500

    Update Task 11.vb

--- Task 11.vb
+++ Task 11.vb
@@ -2,13 +2,13 @@ Module Module1
 
     Sub Main()
         Console.WriteLine("Enter String: ")
-        Dim input As String = Console.ReadLine()
+        Dim input As String = LCase(Console.ReadLine())
 
         For count = 1 To Len(input)
             For count2 = 1 To Len(input)
-                If LCase(Mid(input, count, 3)) = "not" And LCase(Mid(input, count2, 3)) = "bad" Then
+                If Mid(input, count, 3) = "not" And Mid(input, count2, 3) = "bad" Then
                     Dim charsToReplace As Integer = (count2 - count)
-                    input = Left(input, (count - 1)) & "good" & Mid(input, (count2 + 3), (Len(input) - (count2 + 2)))
+                    input = Left(input, (count - 1)) & "good" & Mid(input, (count2 + 3), (Len(input)))
                 End If
             Next
         Next
