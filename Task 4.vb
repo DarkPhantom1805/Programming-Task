da89f935b027c4e80e52fb8f3cd4a8c841a9746c
DarkPhantom1805 <abdullahyousuf1234@gmail.com>
2021-11-16 18:08:29 +0500

    Update Task 4.vb

--- Task 4.vb
+++ Task 4.vb
@@ -1,45 +1,27 @@
-Module Module1
+Module module1
+    Sub main()
+        Console.Write("Enter String: ")
+        Dim Input As String = Console.ReadLine()
 
-    Sub Main()
-        Console.WriteLine("Enter String: ")
-        Dim input As String = Console.ReadLine()
+        Dim char1, highChar As Char
+        Dim maxCount As Integer
 
-        Dim length, highcount(Len(input)), maxHigh As Integer
-        Dim char1(Len(input)) As Char
-        Dim char2(Len(input)) As Char
+        Dim highCount(Len(Input)) As Integer
 
-        For count = 1 To Len(input)
-            char1(count) = Mid(input, count, 1)
-            char2(count) = Mid(input, count, 1)
-            length += 1
-        Next
-
-        Dim char3(length) As Char
-
-        For count = 1 To length
-            highcount(count) = 0
-            If char1(count) = char2(count) Then
-                char3(count) = char1(count)
+        For count = 1 To Len(Input)
+            char1 = Mid(Input, count, 1)
+            highCount(count) = Input.Count(Function(x) LCase(x) = char1) 'this "Input.Count(Function(x) LCase(x) = char1)" is the only
+            ' statement which i dont know how it works. I got the correct count for the most appeared character however my code would
+            ' display the wrong character even while showing the correct repeat count. So I searched stack overflow for a solution and
+            ' found this line which works perfectly. This is also the only help i've taken from the internet in all of my 11 codes
+            If highCount(count) > maxCount Then
+                maxCount = highCount(count)
+                highChar = Mid(Input, count, 1)
             End If
         Next
 
-        For count = 1 To length
-            For count2 = 1 To length
-                If char3(count) = Mid(input, count2, 1) Then
-                    highcount(count) += 1
-                End If
-            Next
-        Next
-
-        For count = 1 To length
-            If highcount(count) > maxHigh Then
-                maxHigh = highcount(count)
-            End If
-        Next
-
-        Console.WriteLine("The character that appears the most is: '" & char3(maxHigh) & "' it appears: '" & maxHigh & "' times.")
+        Console.WriteLine("The character that appears the most is: '" & highChar & "' and it appears: '" & maxCount & "' times.")
 
         Console.ReadLine()
     End Sub
-
 End Module
