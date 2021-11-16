5de4fb494c0998aa8f79241033df6c6d50fe9949
DarkPhantom1805 <abdullahyousuf1234@gmail.com>
2021-11-16 21:51:40 +0500

    Create Task 8.vb

---
+++ VB/Task 8.vb
@@ -0,0 +1,20 @@
+Module Module1
+
+    Sub Main()
+        Console.Write("Enter String: ")
+        Dim input As String = LCase(Console.ReadLine())
+
+        Dim firstChar As Char = Left(input, 1)
+
+        For count = 1 To Len(input)
+            If firstChar = Mid(input, count, 1) Then
+                Mid(input, count, 1) = "*"
+            End If
+        Next
+
+        Console.WriteLine()
+        Console.WriteLine(firstChar & Mid(input, 2, Len(input)))
+        Console.ReadKey()
+    End Sub
+
+End Module
