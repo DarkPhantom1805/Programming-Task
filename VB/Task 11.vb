586f08d7c847f432b2b6ca293537a3fd6e0c5dc8
DarkPhantom1805 <abdullahyousuf1234@gmail.com>
2021-12-01 18:23:24 +0500

    Update Task 11.vb

--- VB/Task 11.vb
+++ VB/Task 11.vb
@@ -7,7 +7,6 @@ Module Module1
         For count = 1 To Len(input)
             For count2 = 1 To Len(input)
                 If Mid(input, count, 3) = "not" And Mid(input, count2, 3) = "bad" Then
-                    Dim charsToReplace As Integer = (count2 - count)
                     input = Left(input, (count - 1)) & "good" & Mid(input, (count2 + 3), (Len(input)))
                 End If
             Next
